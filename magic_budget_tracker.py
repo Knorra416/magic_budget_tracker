a2c45a4c87646933b9830e2a8e920a736f7530f0
alex.knorr <alex.knorr@slalom.com>
2019-02-09 10:32:02 -0700

    working file. Update requirements, update config.

--- magic_budget_tracker.py
+++ magic_budget_tracker.py
@@ -1,6 +1,7 @@
-import pandas as pd
 import json
-import requests
+import scrython
+import pandas as pd
+import argparse as ap
 
 
 def goal_status(budget_file, card_config):
@@ -12,6 +13,57 @@ def goal_status(budget_file, card_config):
     """
 
     data = pd.read_csv(budget_file)
+    assert "Saving" in data.columns, f"{data.columns} missing column named Saving"
+
+    total_savings = sum(data.Saving)
+
+    with open(card_config) as f:
+        card_file = json.load(f)
+
+    card_prices = {}
+    for card in card_file:
+        card_data = scrython.cards.Named(fuzzy=card)
+        price = card_data.scryfallJson["prices"]["usd"]
+        card_prices[card] = float(price)
+
+    total_value_list = []
+    for key, val in card_prices.items():
+        quantity = card_file[key]["Quantity"]
+        total = round(quantity * val, 2)
+        total_value_list.append(total)
+
+    total_value = sum(total_value_list)
+
+    percent = total_savings/total_value
+    percent_pretty = "{0:.0f}%".format(percent * 100)
+
+    return f"you are {percent_pretty} of the way to your goal! Card Prices: {card_prices} and Total: {total_value}"
 
 
+def initialize_parser():
+    """ Initialize argparser
+    """
 
+    argparser = ap.ArgumentParser(
+        description="""provide event dictionary for manual run"""
+    )
+    argparser.add_argument("-budget_file", type=str, help="path to .csv with savings, needs a col named Saving")
+    argparser.add_argument("-card_config", type=str, help="path to .json with config for card names and quantity")
+
+    return argparser
+
+
+def main():
+    p = initialize_parser()
+    args = p.parse_args()
+
+    file = args.budget_file
+    config = args.card_config
+
+    output = goal_status(budget_file=file, card_config=config)
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
