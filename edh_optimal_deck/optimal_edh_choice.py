b3f95108a30926135f0126c7bdf8422e678bee22
alex.knorr <alex.knorr@slalom.com>
2019-08-02 09:56:45 -0600

    updates!

--- edh_optimal_deck/optimal_edh_choice.py
+++ edh_optimal_deck/optimal_edh_choice.py
@@ -1,10 +1,10 @@
 from scipy.optimize import linear_sum_assignment
 
 commanders = {
-    "Kadena, Sinking Sorceror": 1,
-    "Ghired, Conclave Exile": 2,
-    "Angie Falkenrath": 3,
-    "Sevinne, the Chronoclasm": 4
+    0: "Kadena, Sinking Sorceror",
+    1: "Ghired, Conclave Exile",
+    2: "Angie Falkenrath",
+    3: "Sevinne, the Chronoclasm"
 }
 
 Alex = ["Kadena, Sinking Sorceror", "Ghired, Conclave Exile", "Angie Falkenrath", "Sevinne, the Chronoclasm"]
@@ -12,9 +12,16 @@ Adam = []
 George = []
 Mike = []
 
-all_rankings = [[1, 2, 3, 4], [], [], []]
+friends = ["Alex", "Adam", "George", "Mike"]
+
+all_rankings = [[0, 1, 2, 3], [], [], []]
 
 row_ind, col_ind = linear_sum_assignment(all_rankings)
 
 print(f"The optimal column index is: {col_ind}")
-print(f"The optimal row index is: {row_ind}")
+
+optimal_commanders_order = []
+for i in col_ind:
+    optimal_commanders_order.append(commanders[i])
+
+print(f"The optimal commander choices in {friends} order is: {optimal_commanders_order}")
